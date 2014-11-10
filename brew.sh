43a9e91e9f9e41050815b79744e4cee2acacb20e
Sébastien Diemer <diemersebastien@yahoo.fr>
2014-11-10 18:14:49 +0100

    initial commit

---
+++ brew.sh
@@ -0,0 +1,23 @@
+if hash brew 2>/dev/null; then
+    echo "homebrew is already installed"
+    echo "Aborting..."
+    exit 1
+fi
+# install homebrew:
+echo "installing homebrew..."
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew upgrade
+brew doctor
+
+brew install bash
+brew install bash-completion
+brew install ffmpeg
+brew install git
+brew install languagetool
+brew install macvim --override-system-vim
+brew install multimarkdown
+brew install pandoc
+brew install python
+brew install unrar
