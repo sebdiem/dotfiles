a3c39a42f0be27f29760565dd52080822f2ce953
Sébastien Diemer <sebastien.diemer@polyconseil.fr>
2014-12-18 17:23:30 +0100

    karabiner

--- .karabiner/karabiner-import.sh
+++ .karabiner/karabiner-import.sh
@@ -2,12 +2,14 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set remap.commandR2controlL 1
+/bin/echo -n .
 $cli set repeat.wait 10
 /bin/echo -n .
-$cli set repeat.initial_wait 200
+$cli set repeat.initial_wait 300
 /bin/echo -n .
 $cli set private.swap_opening_parenthesis_and_a_f 1
 /bin/echo -n .
-$cli set remap.commandR2controlL 1
+$cli set private.map_space_with_enter 1
 /bin/echo -n .
 /bin/echo
